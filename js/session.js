65f5f0001186078b91f2fbcb2f3a0cf71846e733
Marco Castigliego <marcocast@gmail.com>
2015-03-21 10:15:54 +0000

    adding empty image

--- js/session.js
+++ js/session.js
@@ -12,30 +12,22 @@ $(document).ready(function() {
 
 	var table = $("#container");
 
-	$('#container').masonry({
-		itemSelector : '.item',
-		columnWidth : 200,
-		"gutter" : 10
-
-	});
-
+	
 	ref.child("session-participants").child("mmm").on("value", function(aSnapshot) {
 
 		aSnapshot.forEach(function(childSnapshot) {
 
 			totUsers++;
-
-			// key will be "fred" the first time and "wilma" the second time
 			var key = childSnapshot.key();
+			var keyNoColum = key.replace(":", "");
 
-			// childData will be the actual contents of the child
 			var username = childSnapshot.child("username").val();
 
 			var row = "<div class='item'>";
 			row += "<br>";
 			row += "<strong>" + username + "</strong>";
 			row += "<br>";
-			row += "<div id='" + key.replace(":", "") + "'></div>";
+			row += "<div id='" + keyNoColum + "'></div>";
 			row += "</div>";
 
 			var el = jQuery(row);
@@ -43,20 +35,17 @@ $(document).ready(function() {
 			jQuery("#container").append(el).masonry( 'reload' );
 
 			ref.child("session-votes").child("mmm").child(key).once("value", function(childSnapshot) {
-				// key will be "fred" the first time and "wilma" the second time
-				var key = childSnapshot.key();
-
-				// childData will be the actual contents of the child
 				var card = childSnapshot.child("card").val();
-				var cardImg = "<img src='img/card_back.png' height='160' width='100'>";
+				var cardImg = "<img src='img/cards/blank_card.png' height='160' width='100'>";
 				if (card != null && card != "none") {
+					cardImg = "<img src='img/card_back.png' height='160' width='100'>";
 					totVotes++;
-					mapUserImage[key.replace(":", "")] = "<img src='img/cards/" + cards[parseInt(card)] + ".jpg'>";
-				} else if (totVotes > 0) {
+					mapUserImage[keyNoColum] = "<img src='img/cards/" + cards[parseInt(card)] + ".jpg'>";
+				} else if (totVotes > 0) {				
 					totVotes--;
 				}
 
-				$("#" + key.replace(":", "")).html(cardImg);
+				$("#" + keyNoColum).html(cardImg);
 
 				if (totUsers <= totVotes) {
 					for (var id in mapUserImage) {
@@ -64,14 +53,7 @@ $(document).ready(function() {
 							$("#" + id).html(mapUserImage[id]);
 						}
 					}
-				} else {
-					for (var id in mapUserImage) {
-						if (mapUserImage.hasOwnProperty(id)) {
-							$("#" + id).html(cardImg);
-						}
-					}
-				}
-
+				} 
 			});
 
 		});
@@ -84,9 +66,6 @@ $(document).ready(function() {
 
 	ref.child("session-votes").child("mmm").on("child_changed", function(childSnapshot) {
 		location.reload();
-
-		
-
 	});
 
 });
